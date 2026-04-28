45f67e329963ec5101b56adfd3e4a8ff19c2bf85
karyndevina <karyndevina88@gmail.com>
2026-04-28 16:34:06 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 window.addEventListener('scroll', function() {
     const navbar = document.querySelector('.navbar');
 
-    if (this.window.scrollY > 50) {
+    if (window.scrollY > 50) {
         navbar.classList.add('scrolled');
     } else {
         navbar.classList.remove('scrolled');
